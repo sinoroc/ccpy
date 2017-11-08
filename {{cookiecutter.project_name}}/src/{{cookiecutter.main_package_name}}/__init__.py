37ce1e4b1ad34984db9ffb286216023eab32e067
sinoroc <sinoroc.code+git@gmail.com>
2017-11-08 17:13:12 +0100

    Let pylint warn about useless message suppressions

--- {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/__init__.py
@@ -6,7 +6,7 @@ import pkg_resources
 
 
 # PEP 396
-__version__ = pkg_resources.get_distribution(  # pylint: disable=no-member
+__version__ = pkg_resources.get_distribution(
     '{{cookiecutter.project_name}}',  # http://stackoverflow.com/a/22845276
 ).version
 
