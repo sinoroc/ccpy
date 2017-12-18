4b9a807d8ecdcc451e4e80d4387dcdf96915758d
sinoroc <sinoroc.code+git@gmail.com>
2017-12-18 21:26:34 +0100

    Use 'readme_renderer' to check package metadata
    Having 'docutils' in the 'setup_requires' does not work as smoothly
    as expected (at least in combination with 'tox'). Checking the
    ReStructuredText does not need to be enforced on all
    'setup.py check' operations.

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -31,15 +31,11 @@ REQUIREMENTS_PACKAGE = [
 ]
 
 
-REQUIREMENTS_SETUP = [
-    'docutils',  # needed for 'setup.py check --restructuredtext'
-]
-
-
 REQUIREMENTS_TEST = [
     'pytest',
     'pytest-pep8',
     'pytest-pylint',
+    'readme_renderer',  # for 'setup.py check --restructuredtext'
 ]
 
 
@@ -82,7 +78,6 @@ def _do_setup():
         install_requires=REQUIREMENTS_INSTALL,
         package_dir=package_directories,
         packages=packages,
-        setup_requires=REQUIREMENTS_SETUP,
     )
     return
 
