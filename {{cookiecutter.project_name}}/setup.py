51f06fc8b1ce98720ae56fdc553a638bda834c5e
sinoroc <sinoroc.code+git@gmail.com>
2017-12-18 18:35:43 +0100

    Make the project metadata check more strict

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -13,6 +13,11 @@ NAME = '{{cookiecutter.project_name}}'
 DESCRIPTION = "{{cookiecutter.project_name}} library"
 
 
+AUTHOR = '{{cookiecutter.author}}'
+AUTHOR_EMAIL = '{{cookiecutter.author_email}}'
+URL = '{{cookiecutter.url}}'
+
+
 LICENSE = 'Apache-2.0'  # https://spdx.org/licenses/
 
 
@@ -26,10 +31,16 @@ REQUIREMENTS_PACKAGE = [
 ]
 
 
+REQUIREMENTS_SETUP = [
+    'docutils',  # needed for simple 'python setup.py check --restructuredtext'
+]
+
+
 REQUIREMENTS_TEST = [
     'pytest',
     'pytest-pep8',
     'pytest-pylint',
+    'readme_renderer',  # needed for 'python setup.py check --restructuredtext'
 ]
 
 
@@ -61,14 +72,18 @@ def _do_setup():
         name=NAME,
         version=version,
         # metadata
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
         description=DESCRIPTION,
         license=LICENSE,
         long_description=long_description,
+        url=URL,
         # options
         extras_require=REQUIREMENTS_EXTRAS,
         install_requires=REQUIREMENTS_INSTALL,
         package_dir=package_directories,
         packages=packages,
+        setup_requires=REQUIREMENTS_SETUP,
     )
     return
 
