f49c313cd1e879fae7c23a52ead0f8368dc4570a
sinoroc <sinoroc.code+git@gmail.com>
2020-04-27 17:14:04 +0200

    Add empty lines in 'application' module 'cli'

--- {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/cli.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/cli.py
@@ -12,8 +12,10 @@ def main():
     """CLI main function."""
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO)
+    #
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument('--version', action='version', version=_meta.VERSION)
+    #
     args = parser.parse_args()
     logger.info("Arguments %s", args)
 
