66b11e7e9a7586cbd03b00b35cb295db1216c192
sinoroc <sinoroc.code+git@gmail.com>
2020-07-24 20:03:28 +0200

    Update handling of version number

--- {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/_meta.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/_meta.py
@@ -6,6 +6,8 @@ import importlib_metadata
 
 PROJECT_NAME = '{{cookiecutter.project_name}}'
 
-VERSION = importlib_metadata.version(PROJECT_NAME)
+_DISTRIBUTION_METADATA = importlib_metadata.metadata(PROJECT_NAME)
+
+VERSION = _DISTRIBUTION_METADATA['Version']
 
 # EOF
