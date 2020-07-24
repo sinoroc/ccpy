69602187e2013c34d64d81ba1c44a580e03a59b5
sinoroc <sinoroc.code+git@gmail.com>
2020-07-24 20:47:07 +0200

    Fix type hints

--- {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/main.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.main_package_name}}/main.py
@@ -2,15 +2,23 @@
 
 """Main application module."""
 
+import typing
+
 import pyramid
 import pyramid.config
 
 
-def _root_view(unused_context, unused_request):
+def _root_view(
+        unused_context: typing.Any,
+        unused_request: pyramid.request.Request,
+) -> pyramid.response.Response:
     return pyramid.httpexceptions.HTTPOk()
 
 
-def entry_point(unused_global_config, **settings):
+def entry_point(
+        unused_global_config: typing.Dict[str, str],
+        **settings: typing.Any,
+) -> typing.Any:
     """Application entry point."""
     config = pyramid.config.Configurator(settings=settings)
     config.add_route('root', '/')
--- {{cookiecutter.project_name}}/test/test_functional.py
+++ {{cookiecutter.project_name}}/test/test_functional.py
@@ -12,14 +12,14 @@ import {{cookiecutter.main_package_name}}
 class TestFunctional(unittest.TestCase):
     """ Functional test."""
 
-    def setUp(self):
+    def setUp(self) -> None:
         wsgi_app = {{cookiecutter.main_package_name}}.main.entry_point({})
         self.test_app = webtest.TestApp(wsgi_app)
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         pass
 
-    def test_functional(self):
+    def test_functional(self) -> None:
         """Root URL should return HTPP status code 200."""
         self.test_app.get('/', status=200)
 
