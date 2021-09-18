ae87896c709c6a78340ce573178b251a84a419e8
sinoroc <sinoroc.code+git@gmail.com>
2021-09-18 11:14:44 +0200

    Fix docstrings in 'test/test_functional.py'

--- {{cookiecutter.project_name}}/test/test_functional.py
+++ {{cookiecutter.project_name}}/test/test_functional.py
@@ -10,14 +10,15 @@ import {{cookiecutter.main_package_name}}
 
 
 class TestFunctional(unittest.TestCase):
-    """ Functional test."""
+    """Functional test."""
 
     def setUp(self) -> None:
+        """Set up test."""
         wsgi_app = {{cookiecutter.main_package_name}}.main.entry_point({})
         self.test_app = webtest.TestApp(wsgi_app)
 
     def tearDown(self) -> None:
-        pass
+        """Tear down test."""
 
     def test_functional(self) -> None:
         """Root URL should return HTPP status code 200."""
